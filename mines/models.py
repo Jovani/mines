09f06f4594326be9a133468304bc33ddeb85878c
Ivan Djokic <jovani.ivan@gmail.com>
2018-08-20 13:12:24 -0400

    Added class methods and build_game util module

--- mines/models.py
+++ mines/models.py
@@ -28,7 +28,34 @@ class GameRoom(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
 
+    @classmethod
+    def open_room(cls, name, grid_size, game_difficulty):
+        game = MinesweeperGame.start_game(grid_size, game_difficulty)
+        
+        return cls.objects.create(name=name, state=ROOM_OPEN, game=game)
+
+    def close_room(self):
+        self.state = ROOM_CLOSED
+        self.save()
+
+
 class MinesweeperGame(models.Model):
     difficulty = models.IntegerField(choices=DIFFICULTY_CHOICES)
     game_state = models.CharField()
     grid_size = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 31)])
+
+    @classmethod
+    def start_game(game, grid_size, difficulty):
+        from mines.utils.build_game import build_grid
+
+        # A difficulty map to translate semantic difficulty into average percentage of bombs on a field.
+        difficulty_map = {
+            'easy': 20,
+            'medium': 40,
+            'hard': 60
+        }
+        difficulty_value = difficulty_map.get(difficulty, 40)  # Default difficulty of Medium
+
+        # Create a grid of <grid_size x grid_size> 
+        grid = build_grid(grid_size, difficulty_value)
+
---
+++ mines/utils/build_game.py
@@ -0,0 +1,71 @@
+'''
+    Helper module for building a minesweeper game
+'''
+
+import random
+from functools import reduce
+
+
+'''
+    Helper methods for building the grid and setting the bombs
+'''
+def build_grid(grid_size, difficulty):
+    # Build a grid of bombs and non-bombs
+    base_grid = [build_row(grid_size, difficulty) for x in range(grid_size)]
+
+    # With the bombs now set, go over the grid again and determine the values of the non-bomb squares
+    filled_grid =  fill_grid(base_grid)
+
+def build_row(grid_size, difficulty):
+    return [build_column(grid_size, difficulty) for x in range(grid_size)]
+
+def build_column(grid_size, difficulty):
+    is_bomb = random.randrange(0, 100) < difficulty
+    return {
+        'flipped': False,
+        'value': -1 if is_bomb else 0
+    }
+
+'''
+    Helper methods for filling the values of non-bomb squares
+'''
+def fill_grid(base_grid):
+    return [fill_column(base_grid, i, column) for i, column in enumerate(base_grid)]
+
+def fill_column(base_grid, index, column):
+    return [compute_square_value(base_grid, index, j, square) for j, square in enumerate(column)]
+
+def compute_square_value(base_grid, index_i, index_j, square):
+    # Each non-bomb square's value must be equal to the number of bombs surrounding it.
+    if square['value'] == -1:
+        return square
+
+    # do a shallow copy of the square, to avoid mutating
+    new_square = dict(square)
+
+    search_list = [
+        (index_i - 1, index_j - 1), (index_i - 1, index_j), (index_i - 1, index_j + 1),
+        (index_i, index_j - 1), (index_i, index_j + 1),
+        (index_i + 1, index_j - 1), (index_i + 1, index_j), (index_i + 1, index_j + 1),
+    ]
+
+    value = reduce(
+        lambda value, pair: value + 1 if is_bomb_square(base_grid, *pair) else 0,
+        search_list,
+        0
+    )
+
+    new_square['value'] = value
+    return new_square
+
+def is_bomb_square(base_grid, x, y):
+    if x < 0 or y < 0:
+        return
+    
+    try:
+        other_square = base_grid[x][y]
+    except IndexError:
+        pass
+    else:
+        if other_square['value'] == -1:
+            return True
