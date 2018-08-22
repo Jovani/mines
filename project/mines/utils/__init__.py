982007aa5deebf1d7fdc997c0701cc610a45e504
Ivan Djokic <jovani.ivan@gmail.com>
2018-08-22 11:07:55 -0400

    New helper function to get squares directly contiguous to source square

--- project/mines/utils/__init__.py
+++ project/mines/utils/__init__.py
@@ -16,3 +16,12 @@ def get_search_list(x, y):
         (x + 1, y),
         (x + 1, y + 1),
     ]
+
+# Helper function to get (up to) all 4 squares directly bordering the given square.
+def get_chain_trigger_list(x, y):
+    return [
+        (x - 1, y),
+        (x, y - 1),
+        (x, y + 1),
+        (x + 1, y),
+    ]
